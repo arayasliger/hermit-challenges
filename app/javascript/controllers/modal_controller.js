66c3070113ffc2c8f0df92774cc34771432cf2f0
Araya <arayasliger12@gmail.com>
2025-01-17 12:43:29 -0700

    Refactor code and added edit coords modal and delete function

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,16 +2,31 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  static targets = ["modal"];
+  static targets = ["addModal", "editModal"];
   
   connect() {
-    $(this.modalTarget).modal({
+    $(this.addModalTarget).modal({
+      detachable: false,
+      inverted: true
+    });
+
+    $(this.editModalTarget).modal({
       detachable: false,
       inverted: true
     });
   }
 
-  show() {
-    $(this.modalTarget).modal("show");
+  show(event) {
+    event.preventDefault();
+
+    const modalType = event.target.dataset.modal;
+
+    if (modalType === "addModal") {
+      const addModal = this.addModalTarget;
+      $(addModal).modal("show");
+    } else if (modalType === "editModal") {
+      const editModal = this.editModalTarget;
+      $(editModal).modal("show");
+    }
   }
 }
